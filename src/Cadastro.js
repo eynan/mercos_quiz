1f156d229975a20d0edf886aa9d78afd353df20d
Renan Roger <renan@meuspedidos.com.br>
2019-07-26 18:02:54 -0300

    criando cadastro e removendo .ide

---
+++ src/Cadastro.js
@@ -0,0 +1,12 @@
+import React, {Fragment} from 'react'
+
+const Pagina = () => (
+    <Fragment>
+        <label>Nome</label>
+        <input type='text'/>
+        <label>E-mail</label>
+        <input type='text'/>
+        <label>Telefone</label>
+        <input type='text'/>
+    </Fragment>
+)

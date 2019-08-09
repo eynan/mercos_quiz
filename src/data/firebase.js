065b35c1c1769704bd7ce66d9c6ec2a8808283b4
Renan Roger <renan@meuspedidos.com.br>
2019-08-09 19:33:11 -0300

    Update firebase.js

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -1,13 +1,2 @@
 
-    // Your web app's Firebase configuration
-    var firebaseConfig = {
-      apiKey: "AIzaSyD8He6KFO3uY2DH7bzpY84dw3futtrJ2UM",
-      authDomain: "quizmercos.firebaseapp.com",
-      databaseURL: "https://quizmercos.firebaseio.com",
-      projectId: "quizmercos",
-      storageBucket: "",
-      messagingSenderId: "25997856780",
-      appId: "1:25997856780:web:37f8b28d3aa52c0f"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
+

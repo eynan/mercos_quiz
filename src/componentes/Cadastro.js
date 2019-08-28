ee17fd29d96c5aa31f9142fb0bf679ac1eb81dbc
Renan Roger <eynanx@gmail.com>
2019-08-28 00:51:50 -0300

    alterando cadastro

--- src/componentes/Cadastro.js
+++ src/componentes/Cadastro.js
@@ -1,5 +1,8 @@
 import React, {useState} from 'react'
 import styles from './Cadastro.module.css'
+import Select from './Select'
+import Toggle from 'react-toggle'
+import "react-toggle/style.css"
 
 const Cadastro = (props) => {
 
@@ -9,7 +12,9 @@ const Cadastro = (props) => {
         telefone: '',
         trabalhaComOque: '',
         trabalhandoAtualmente: '',
-        evento: 'code-con'
+        evento: 'code-con',
+        conheceMercos: '',
+        aceitaContato: true
     })
 
     const enviarCadastro = () => {
@@ -44,7 +49,7 @@ const Cadastro = (props) => {
                     value={cadastro.telefone}
                     onChange={event => setCadastro({...cadastro, telefone: event.target.value})}
                 />
-                <label className={styles.labelCadastro}>Com o que você trabalha?</label>
+                <label className={styles.labelCadastro}>Com o que você está trabalhando?</label>
                 <input
                     type='text'
                     placeholder='FullStack, FrontEnd, Mobile, DevOps...'
@@ -52,13 +57,29 @@ const Cadastro = (props) => {
                     value={cadastro.trabalhaComOque}
                     onChange={event => setCadastro({...cadastro, trabalhaComOque: event.target.value})}
                 />
-                <label className={styles.labelCadastro}>Onde você trabalha hoje?</label>
+                <label className={styles.labelCadastro}>Em qual empresa trabalha?</label>
                 <input
                     type='text'
                     className={styles.inputCadastro}
                     value={cadastro.trabalhandoAtualmente}
                     onChange={event => setCadastro({...cadastro, trabalhandoAtualmente: event.target.value})}
                 />
+                <label className={styles.labelCadastro}>Já conhecia a Mercos?</label>
+                <Select
+                    items={[{data:{title: 'SIM'}}, {data:{title: 'Não'}}]}
+                    doFilter={respostas => setCadastro({...cadastro, conheceMercos: respostas[0]})}
+                >
+                    <span className='real-placeholder'>Selecione...</span>
+                </Select>
+                <label className={styles.contatoMercos}>
+                    <span className={styles.labelCadastro} style={{marginRight: '10px', marginTop: '5px'}}>Aceita que a Mercos entre em contato sobre carreira e vagas?</span>
+                    <Toggle
+                        defaultChecked={cadastro.aceitaContato}
+                        icons={false}
+                        onChange={() => setCadastro({...cadastro, aceitaContato: !cadastro.aceitaContato})} 
+                    />
+                </label>
+                
                 <button className={styles.botaoAcao} onClick={() => enviarCadastro()}> Cadastrar!</button>
             </div>
         </div>
